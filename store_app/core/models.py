1d006322265a0e1fa3064c25fd1f9211078aa1af
yahya <yahyaest1000@gmail.com>
2024-04-01 23:56:56 +0100

    Implement django websocket channel for notification triggered by signal - Part2

--- store_app/core/models.py
+++ store_app/core/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+
+class WebsocketSignalTrigger(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    socket_data = models.JSONField(blank=True, null=True)
