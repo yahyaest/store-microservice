a10d4b0bfce2fed954a30c53e69cb1acf596ac7e
yahya <yahyaest1000@gmail.com>
2024-04-14 13:00:44 +0100

    Implement Order Filter

--- store_app/api/filters.py
+++ store_app/api/filters.py
@@ -1,5 +1,4 @@
-from dataclasses import fields
-from .models import Product
+from .models import Product, Order
 from django_filters.rest_framework import FilterSet
 
 class ProductFilter(FilterSet):
@@ -9,4 +8,12 @@ class ProductFilter(FilterSet):
             'collection_id':['exact'],
             'slug':['exact'],
             'price':['gt','lt']
+        }
+
+class OrderFilter(FilterSet):
+    class Meta:
+        model = Order
+        fields = {
+            'customer_name': ['exact'],
+            'customer_email' : ['exact']
         }
